503044094fa92d947249ef9d292c1008ce0c1b7a
Avinash Mohanta <avinash.mohanta@blackstraw.ai>
2021-03-27 11:54:25 +0000

    * App signing configs  - added certs and profiles for distribution and development signing  - added assets required for app store distribution  - updated app and framework bundle name  - App dependencies: updated dependencies  -Updated app icons

--- src/OrderScrapper/OrderScrapper/Constants/AppConstants.swift
+++ src/OrderScrapper/OrderScrapper/Constants/AppConstants.swift
@@ -6,8 +6,8 @@
 import Foundation
 
 struct AppConstants {
-    static let bundle: Bundle! = Bundle(identifier: "ai.blackstraw.orderscrapper.OrderScrapper")
-    static let identifier: String  = "ai.blackstraw.orderscrapper.OrderScrapper"
+    static let bundle: Bundle! = Bundle(identifier: "ai.blackstraw.receiptstrawlib.dev")
+    static let identifier: String  = "ai.blackstraw.receiptstrawlib.dev"
     static let numberOfSteps: Float = 6
     static let entityName = "UserAccount"
     static let resource = "CoreDataModel"
