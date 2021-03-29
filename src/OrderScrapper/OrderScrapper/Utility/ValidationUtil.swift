020b1266493a02a57980527dd75e8e3e66a7528a
sagar kulthe <sagar.kulthe@blackstraw.ai>
2021-03-29 10:48:07 +0000

    - Mobile number validation updated

--- src/OrderScrapper/OrderScrapper/Utility/ValidationUtil.swift
+++ src/OrderScrapper/OrderScrapper/Utility/ValidationUtil.swift
@@ -7,16 +7,19 @@ import Foundation
 
 class ValidationUtil {
     static let emailRegEx = "[A-Z0-9a-z.-_]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,3}"
+    static let mobileNoRegex = "^[+]?[0-9]{10,13}$"
     
     static func isValidEmail(email: String) -> Bool {
         let emailTest = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
-        if !email.isEmpty && emailTest.evaluate(with: email) {
-            return true
+        let mobileTest = NSPredicate(format: "SELF MATCHES %@", mobileNoRegex)
+        if !emailTest.evaluate(with: email) && !mobileTest.evaluate(with: email) {
+            return false
         }
-        return false
+        return true
     }
     
     static func isValidPassword(password: String) -> Bool {
         return !password.isEmpty
     }
 }
+
