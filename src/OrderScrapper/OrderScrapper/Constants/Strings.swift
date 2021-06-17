322249249c12178acb2f0d5c39f653b3bba62ed7
Ameyr23 <84459861+Ameyr23@users.noreply.github.com>
2021-06-17 12:33:16 +0530

    Modified the Auth error msg and SDK version (#105)

--- src/OrderScrapper/OrderScrapper/Constants/Strings.swift
+++ src/OrderScrapper/OrderScrapper/Constants/Strings.swift
@@ -35,7 +35,7 @@ struct Strings {
     // Mark:- Non-Localized strings
     static let ErrorLibAlreadyInitialized = "OrdersExtractor is already initialized."
     static let ErrorLibNotInitialized = "OrdersExtractor is not initialized."
-    static let ErrorAuthProviderNotImplemented = "AuthProvider implementation is missing."
+    static let ErrorAuthProviderNotImplemented = "AuthProvider missing authToken or panelistId ."
     static let ExtractionDisabled = "Receipts scrapped already."
     static let ErrorUserAbortedProcess = "User aborted the process."
     static let ErrorConfigsMissing = "Configs missing"
