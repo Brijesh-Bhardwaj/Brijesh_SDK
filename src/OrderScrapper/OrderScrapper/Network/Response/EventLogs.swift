c205472a8f940e6cd966f238d35bbaa203421ffe
sagarkulthe-iritech <81735595+sagarkulthe-iritech@users.noreply.github.com>
2021-12-14 20:16:56 +0530

    Scrapping context pass in push events api (#325)

--- src/OrderScrapper/OrderScrapper/Network/Response/EventLogs.swift
+++ src/OrderScrapper/OrderScrapper/Network/Response/EventLogs.swift
@@ -4,7 +4,7 @@
 import Foundation
 
 enum EventJsonKey: String {
-    case panelistId, toDate, fromDate, platformId, section, status, message, type, scrappingType
+    case panelistId, toDate, fromDate, platformId, section, status, message, type, scrappingType, scrapingContext
 }
 
 struct EventLogs: Codable {
@@ -21,7 +21,7 @@ struct EventLogs: Codable {
     
     
     func toDictionary() -> [String: Any] {
-        return [EventJsonKey.panelistId.rawValue: panelistId.lowercased(), EventJsonKey.section.rawValue: section, EventJsonKey.toDate.rawValue: toDate as Any, EventJsonKey.fromDate.rawValue: fromDate as Any, EventJsonKey.status.rawValue: status,EventJsonKey.message.rawValue: message, EventJsonKey.type.rawValue: type, EventJsonKey.platformId.rawValue: platformId.lowercased(), EventJsonKey.scrappingType.rawValue: scrapingType as Any]
+        return [EventJsonKey.panelistId.rawValue: panelistId.lowercased(), EventJsonKey.section.rawValue: section, EventJsonKey.toDate.rawValue: toDate as Any, EventJsonKey.fromDate.rawValue: fromDate as Any, EventJsonKey.status.rawValue: status,EventJsonKey.message.rawValue: message, EventJsonKey.type.rawValue: type, EventJsonKey.platformId.rawValue: platformId.lowercased(), EventJsonKey.scrappingType.rawValue: scrapingType as Any, EventJsonKey.scrapingContext.rawValue: scrapingContext?.lowercased() as Any]
     }
 }
 
