d7c8e512c1e64644fce26aa4bce963c0d2abc1aa
mayursblackstraw <81749948+mayursblackstraw@users.noreply.github.com>
2022-02-28 17:13:38 +0530

    Qa multi retailers (#435)
    - Added changes for the incentive logic when clicked on donwload online orders button passed type as manual if the flag is false

--- src/OrderScrapper/OrderScrapper/Common/LibContext.swift
+++ src/OrderScrapper/OrderScrapper/Common/LibContext.swift
@@ -30,4 +30,5 @@ class LibContext {
     var scrapeCompletionPublisher = PassthroughSubject<((Bool, OrderFetchSuccessType?), ASLException?), Never>()
     var webAuthErrorPublisher = PassthroughSubject<(Bool, String), Never>()
     var authenticationErrorPublisher = PassthroughSubject<Bool, Never>()
+    var isIncetiveFlag: Bool = false
 }
